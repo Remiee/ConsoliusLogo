2f2762877ab886fb5004c4593f78f38ebd50ba83
Remiee <nemes.agi96@gmail.com>
2018-08-25 11:07:47 +0200

    inputhandler modification

--- src/consoliuslogo/InputHandler.java
+++ src/consoliuslogo/InputHandler.java
@@ -6,29 +6,27 @@ import consoliuslogo.enums.Rotate;
 import java.util.Scanner;
 
 public class InputHandler {
-    private Rotate rotate;
-    private int rotateBy;
-    private boolean isDrawing;
-    private Direction direction;
-    private int distance;
+    private Scanner scanner;
     private String[] instructions;
 
-    public Rotate getRotate(Scanner scanner) {
+    public InputHandler(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Rotate getRotate() {
         System.out.println("Add meg, hogy szeretnél-e fordulni. R(jobbra), L(balra)?");
-        rotate = null;
         char input = scanner.next().charAt(0);
         while (input != 'R' && input != 'L') {
             System.out.println("A megadott érték nem megfelelő.");
             input = scanner.next().charAt(0);
         }
-        rotate = input == 'R' ? Rotate.RIGHT : Rotate.LEFT;
 
-        return rotate;
+        return input == 'R' ? Rotate.RIGHT : Rotate.LEFT;
     }
 
-    public int getDegree(Scanner scanner) {
+    public int getDegree() {
         System.out.println("Add meg, hogy hány fokot szeretnél fordulni.");
-        rotateBy = scanner.nextInt();
+        int rotateBy = scanner.nextInt();
         while (rotateBy % 90 != 0) {
             System.out.println("A megadott érték nem megfelelő.");
             rotateBy = scanner.nextInt();
@@ -37,32 +35,30 @@ public class InputHandler {
         return rotateBy;
     }
 
-    public boolean getIsDrawing(Scanner scanner) {
+    public boolean getIsDrawing() {
         System.out.println("Akarod-e, hogy rajzoljon a teknős? Y(igen) N(nem)");
-        char draw = scanner.next().charAt(0);
-        while (draw != 'Y' && draw != 'N') {
+        char input = scanner.next().charAt(0);
+        while (input != 'Y' && input != 'N') {
             System.out.println("A megadott érték nem megfelelő.");
-            draw = scanner.next().charAt(0);
+            input = scanner.next().charAt(0);
         }
-        isDrawing = draw == 'Y' ? true : false;
-        return isDrawing;
+        return input == 'Y' ? true : false;
     }
 
-    public Direction getDirection(Scanner scanner) {
+    public Direction getDirection() {
         System.out.println("Add meg, hogy F(előre) vagy B(hátra) szeretnél menni.");
         char input = scanner.next().charAt(0);
         while (input != 'F' && input != 'B') {
             System.out.println("A megadott érték nem megfelelő.");
             input = scanner.next().charAt(0);
         }
-        direction = input == 'F' ? Direction.FORWARD : Direction.BACKWARD;
 
-        return direction;
+        return input == 'F' ? Direction.FORWARD : Direction.BACKWARD;
     }
 
-    public int getDistance(Scanner scanner) {
+    public int getDistance() {
         System.out.println("Add meg, hogy mennyit szeretnél menni.");
-        distance = scanner.nextInt();
+        int distance = scanner.nextInt();
         while (distance < 0) {
             System.out.println("A megadott érték nem megfelelő.");
             distance = scanner.nextInt();
@@ -70,7 +66,7 @@ public class InputHandler {
         return distance;
     }
 
-    public void exit(Scanner scanner) {
+    public void exit() {
         System.out.println("Ki szeretnél lépni a játékból? Y(igen) N(nem)");
         char exit = scanner.next().charAt(0);
         while (exit != 'Y' && exit != 'N') {
@@ -80,5 +76,11 @@ public class InputHandler {
         if (exit == 'Y') {
             System.exit(0);
         }
+    }
+
+    public void getInformation() {
+        System.out.println("Szia! Várom az utasításaidat!");
+        String instruction = scanner.nextLine();
+        instructions = instruction.split(" ");
     }
 }
--- src/consoliuslogo/Main.java
+++ src/consoliuslogo/Main.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        InputHandler inputHandler = new InputHandler();
+        Scanner scanner = new Scanner(System.in);
+        InputHandler inputHandler = new InputHandler(scanner);
         Table table = new Table(20, 20);
         Turtle turtle = new Turtle(10, 10);
-        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             table.paintTurtle(turtle);
             table.printTable();
-            inputHandler.exit(scanner);
-            Command command = new Command(
-                    inputHandler.getRotate(scanner),
-                    inputHandler.getDegree(scanner),
-                    inputHandler.getDirection(scanner),
-                    inputHandler.getDistance(scanner),
-                    inputHandler.getIsDrawing(scanner),
-                    1);
+            inputHandler.exit();
+            Command command = new Command(inputHandler.getRotate(),
+                                          inputHandler.getDegree(),
+                                          inputHandler.getDirection(),
+                                          inputHandler.getDistance(),
+                                          inputHandler.getIsDrawing(),
+                                         1);
             turtle.executeCommand(command, table);
         }
     }
